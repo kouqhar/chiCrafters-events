21681d18704791245c0cdd826bdde57ec24903e2
kouqhar <naphtaliduniya2@gmail.com>
2024-11-11 07:36:48 +0100

    vercel config2

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import vercel from "vite-plugin-vercel";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vercel()],
+  vercel: {
+    rewrites: [
+      {
+        source: "/(.*)",
+        destination: "/index.html",
+      },
+    ],
+  },
 });
